d308afe5ab5e2f14a4cca1a58a0a7575c11be19e
MissingLink <gjq763299969@gmail.com>
2015-02-03 01:48:09 +0800

    add 18 to 22

---
+++ CppPrimer/Chapter13/13_18/13_18.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+class Employee {
+public:
+    static unsigned idGenerator;
+    std::string name;
+    unsigned id;
+public:
+    Employee( ) :name(""), id(++idGenerator) { }
+    Employee(const std::string & s) :name(s), id(++idGenerator) { }
+
+};
+unsigned Employee::idGenerator = 0;
+
